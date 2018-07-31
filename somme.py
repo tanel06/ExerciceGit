53a0b186a9524ea257194d7c3c959f67fcdf1f76
jrazafim <tanel06@yahoo.fr>
2018-07-31 14:58:30 +0200

    Modif somme.py : Ajout de la recursivité

--- somme.py
+++ somme.py
@@ -1,11 +1,22 @@
 def somme(a,b):
     return a + b
 
-def affiche_result(a,b):
-    s = '{} + {} = {}'.format(a,b,somme(a,b))
-    print(s)
+def affiche_result(a,b,s):
+    ch = '{} + {} = {}'.format(a,b,s)
+    print(ch)
 
 
-affiche_result(4, 5)
-affiche_result(11, 10)
-affiche_result(3,3)
+def somme_rec(a,b):
+    if b==0:
+        return a
+    elif a== 0:
+        return b
+    else:
+        return somme_rec(a+1,b-1)
+
+
+affiche_result(4, 5, somme(4,5))
+affiche_result(11, 10, somme(11,10))
+affiche_result(3,3, somme(3,3))
+affiche_result(7,3, somme_rec(7,3))
+
